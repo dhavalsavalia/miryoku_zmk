d0fc316aebb0228c63a95828bee50c135adb306f
Dhaval Savalia <hello@dhavalsavalia.com>
2024-11-09 23:17:07 -0500

    Add custom configurations for Mac clipboard, Workman layout, and Vi navigation

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -1,3 +1,11 @@
 // Copyright 2021 Manna Harbour
 // https://github.com/manna-harbour/miryoku
 
+// use mac clipboard
+#define MIRYOKU_CLIPBOARD_MAC
+
+// use workman layout
+#define MIRYOKU_ALPHAS_WORKMAN
+
+// use vi navigation
+#define MIRYOKU_NAV_VI
