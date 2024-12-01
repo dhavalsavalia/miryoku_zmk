7a9811ab570630efdc40ecbf05563bcdd532f295
Dhaval Savalia <hello@dhavalsavalia.com>
2024-12-01 14:46:38 -0500

    add hyper and meh keys

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -13,15 +13,19 @@
 // Add outer columns to the left and right
 #define XXX &none
 
+// Add hyper and meh keys
+#define HYP LS(LC(LA(LGUI)))
+#define MEH LS(LC(LALT))
+
 #define MIRYOKU_LAYOUTMAPPING_CORNE( \
      K00, K01, K02, K03, K04,      K05, K06, K07, K08, K09, \
      K10, K11, K12, K13, K14,      K15, K16, K17, K18, K19, \
      K20, K21, K22, K23, K24,      K25, K26, K27, K28, K29, \
      N30, N31, K32, K33, K34,      K35, K36, K37, N38, N39 \
 ) \
-&kp ESC  K00  K01  K02  K03  K04       K05  K06  K07  K08  K09  &kp BSLH \
-&kp TAB K10  K11  K12  K13  K14       K15  K16  K17  K18  K19  &kp SQT \
-&kp LSHFT  K20  K21  K22  K23  K24       K25  K26  K27  K28  K29  &kp RSHFT \
+&kp MEH  K00  K01  K02  K03  K04       K05  K06  K07  K08  K09  &kp MEH \
+&kp HYP K10  K11  K12  K13  K14       K15  K16  K17  K18  K19  &kp SQT \
+&kp HYP  K20  K21  K22  K23  K24       K25  K26  K27  K28  K29  &kp HYP \
                K32  K33  K34       K35  K36  K37
 
 #define MIRYOKU_KLUDGE_TOPROWCOMBOS_LEFTPINKIE 2 3
@@ -40,6 +44,6 @@
 // layer moodifications
 #define MIRYOKU_LAYER_BASE \
 &kp Q,             &kp W,             &kp E,             &kp R,             &kp T,             &kp Y,             &kp U,             &kp I,             &kp O,             &kp P,             \
-U_MT(LGUI, A),     U_MT(LALT, S),     U_MT(LCTRL, D),    U_MT(LSHFT, F),    &kp G,             &kp H,             U_MT(LSHFT, J),    U_MT(LCTRL, K),    U_MT(LALT, L),     U_MT(LGUI, SEMI),   \
+U_MT(LGUI, A),     U_MT(LALT, S),     U_MT(LCTRL, D),    U_MT(LSHFT, F),    U_MT(HYP, G),      U_MT(HYP, H),      U_MT(LSHFT, J),    U_MT(LCTRL, K),    U_MT(LALT, L),     U_MT(LGUI, SEMI),   \
 U_LT(U_BUTTON, Z), U_MT(RALT, X),     &kp C,             &kp V,             &kp B,             &kp N,             &kp M,             &kp COMMA,         U_MT(RALT, DOT),   U_LT(U_BUTTON, SLASH),\
 U_NP,              U_NP,              U_LT(U_MEDIA, ESC),U_LT(U_NAV, SPACE),U_LT(U_MOUSE, TAB),U_LT(U_SYM, RET),  U_LT(U_NUM, BSPC), U_LT(U_FUN, DEL),  U_NP,              U_NP
